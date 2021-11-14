8356f305b08790bed2d2f39447a7353790b9889d
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-11-14 16:35:52 +0530

    i still dont know why its not working

--- __tests__/edit_message.py
+++ __tests__/edit_message.py
@@ -1,21 +1,21 @@
 from jak_python_package.edit_message import EditMessage
 
 
-def remove_spaces():
+def test_remove_spaces():
     message = EditMessage("heLlo woRLd")
     assert message.remove_spaces() == "heLlowoRLd"
 
 
-def to_lower_case():
+def test_to_lower_case():
     message = EditMessage("heLlo woRLd")
     assert message.to_lower_case() == "hello world"
 
 
-def to_upper_case():
+def test_to_upper_case():
     message = EditMessage("heLlo woRLd")
     assert message.to_upper_case() == "HELLO WORLD"
 
 
-def to_title_case():
+def test_to_title_case():
     message = EditMessage("heLlo woRLd")
     assert message.to_title_case() == "Hello World"
