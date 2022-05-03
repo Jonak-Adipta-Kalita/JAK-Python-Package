169717608c5e5f31f589a3849083c596371dc508
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-05-03 12:18:50 +0530

    i just leaked it.....

--- __tests__/test_api.py
+++ __tests__/test_api.py
@@ -1,39 +1,45 @@
 from jak_python_package.api import API
 
-RAPIDAPI_KEY = "d3474315fcmshe6f03eb826f16bcp181f82jsn7d262d2601be"
+RAPIDAPI_KEY = "YOUR_RAPIDAPI_KEY"
 
 
 def test_get_jak():
     jak_api = API(rapidapi_key=RAPIDAPI_KEY)
 
     assert jak_api.get_jak()
+    assert type(jak_api.get_jak()) == dict
 
 
 def test_get_brawl_stars():
     jak_api = API(rapidapi_key=RAPIDAPI_KEY)
 
     assert jak_api.get_brawl_stars()
+    assert type(jak_api.get_brawl_stars()) == dict
 
 
 def test_get_miraculous():
     jak_api = API(rapidapi_key=RAPIDAPI_KEY)
 
     assert jak_api.get_miraculous()
+    assert type(jak_api.get_miraculous()) == dict
 
 
 def test_get_mughal_empire():
     jak_api = API(rapidapi_key=RAPIDAPI_KEY)
 
     assert jak_api.get_mughal_empire()
+    assert type(jak_api.get_mughal_empire()) == dict
 
 
 def test_get_genshin_impact():
     jak_api = API(rapidapi_key=RAPIDAPI_KEY)
 
     assert jak_api.get_genshin_impact()
+    assert type(jak_api.get_genshin_impact()) == dict
 
 
 def test_get_alexis_response():
     jak_api = API(rapidapi_key=RAPIDAPI_KEY)
 
     assert jak_api.get_alexis_response("Hello!!")
+    assert type(jak_api.get_alexis_response("Hello!!")) == dict
